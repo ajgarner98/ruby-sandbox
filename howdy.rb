bb54e2faa8104be9763a772a3cf0ae2a34c0c534
Andre Garner <111984130+ajgarner98@users.noreply.github.com>
2023-08-04 17:42:45 +0000

    tried active support

--- howdy.rb
+++ howdy.rb
@@ -1,2 +1,21 @@
 my_string = "Hello World!"
 pp my_string
+
+require "./goodbye.rb"
+require "active_support/all"
+
+1.ordinalize    # => "1st"
+2.ordinalize    # => "2nd"
+53.ordinalize   # => "53rd"
+2009.ordinalize # => "2009th"
+-21.ordinalize  # => "-21st"
+-134.ordinalize # => "-134th"
+
+"table".pluralize     # => "tables"
+"ruby".pluralize      # => "rubies"
+"equipment".pluralize # => "equipment"
+
+"tables".singularize    # => "table"
+"rubies".singularize    # => "ruby"
+"equipment".singularize # => "equipment"
+
